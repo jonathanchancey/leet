95491117c186105d99aaf6f6cb886d1a5be18a0e
Jonathan Chancey <jchancey@ucmerced.edu>
2019-07-23 10:39:22 -0700

    create and finish riot hitcounter

---
+++ hitcounter/jon/hitcounter.py
@@ -0,0 +1,33 @@
+import time
+
+class hitcounter:
+    hit_times = [ ]
+
+
+    def record(self, timestamp):
+        self.hit_times.append(timestamp)
+
+    def total(self):
+        return len(self.hit_times)
+
+    def rang(self, lower, upper):
+        count = 0
+        for hit in self.hit_times:
+            if hit > lower and hit < upper:
+                count = count + 1
+        return count
+
+
+def main():
+    HCo = hitcounter()
+
+    HCo.record(int(time.time()))
+    HCo.record(int(time.time()))
+    HCo.record(int(time.time()))
+    HCo.record(int(time.time()))
+
+    print("HCo.total()", HCo.total())
+    print("Range test", HCo.rang(int(time.time())-20,int(time.time())+20))
+
+if __name__ == '__main__':
+    main()
