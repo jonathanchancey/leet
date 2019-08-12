eacb55e8b12fb5398ddfc8b77d9f9b00599d2335
Jonathan Chancey <jchancey@ucmerced.edu>
2019-08-12 10:35:39 -0700

    added comments

--- nprob/chancey/nprob.py
+++ nprob/chancey/nprob.py
@@ -1,26 +1,27 @@
+# returns random value based on declared percentages
+
 import random
 
 
 def main(nums,per):
+    # generates random value between 0 and 1
     randint = random.uniform(0, 1)
 
-    # based on decimal 
-    per_expanded = [int(i * 100) for i in per]
-    # per_sum = [sum(per[0:i]) for i in range(per)]
     per_sum = []
-
+    # uses a simple loop to sum elements into a new array
     j = 0
     for i in per:
         j = j + i
         per_sum.append(j) 
 
-
+    # compares the randomly generated value to the sumed percentages for pseudo random selection
     for i in per_sum:
         if i > randint:
             return i
 
 
 if __name__ == '__main__':
+    # given values for testing
     nums = [1, 2, 3, 4]
     per = [0.1, 0.5, 0.2, 0.2]
     
