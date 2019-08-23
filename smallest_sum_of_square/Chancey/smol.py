abfc9409bede610b638416019514dd7c74f73590
Jonathan Chancey <jchancey@ucmerced.edu>
2019-08-23 11:17:37 -0700

    Make smol smaller

--- smallest_sum_of_square/Chancey/smol.py
+++ smallest_sum_of_square/Chancey/smol.py
@@ -1,14 +1,10 @@
 import math
+n=13
+r=[]
 def s(c, n):
-    u = sum(i**2 for i in c)
-    if u == n:
+    u=sum(i**2 for i in c)
+    if u==n:
         return len(c)
     c.append(int(math.sqrt(n-u)))
-    return s(c, n)
-def main():
-    n = 13
-    risto = []
-    count = s(risto, n)
-    print("#{}={}".format(n, count))
-if __name__ == '__main__':
-    main()
+    return s(c,n)
+print(s(r,n))
