89840dfb8809910a0395c29a963820fea300be4d
Jonathan Chancey <jchancey@ucmerced.edu>
2019-08-23 11:19:47 -0700

    Remove Tabs and add 1-line if

--- smallest_sum_of_square/Chancey/smol.py
+++ smallest_sum_of_square/Chancey/smol.py
@@ -2,9 +2,8 @@ import math
 n=13
 r=[]
 def s(c,n):
-    u=sum(i**2 for i in c)
-    if u==n:
-        return len(c)
-    c.append(int(math.sqrt(n-u)))
-    return s(c,n)
+	u=sum(i**2 for i in c)
+	if u==n: return len(c)
+	c.append(int(math.sqrt(n-u)))
+	return s(c,n)
 print(s(r,n))
